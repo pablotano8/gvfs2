160f218d310f60d98e5c217251a6629717525c2f
pablotanoretamales <pablotanoretamales@gmail.com>
2022-03-11 19:39:28 +0100

    1st

--- agent.py
+++ agent.py
@@ -8,6 +8,8 @@ from tabular import GVFTable
 
 
 class Agent:
+    """Acts in the environment and trains the gvfs and the actor"""
+
     def __init__(self, env, epsilon_explo=0.05, state=None, exploration=False) -> None:
         self.state = state
         self.epsilon_explo = epsilon_explo
@@ -17,6 +19,7 @@ class Agent:
         self.beh = 0
 
     def controller(self, gvfs: GVFTable, threshold=0.05):
+        """The controller chooses the action based on the gvfs"""
         if self.exploration is True or np.random.uniform(0, 1) < self.epsilon_explo:
             action = np.random.choice(range(self.num_actions))
         else:
@@ -44,6 +47,7 @@ class Agent:
         actor_optimizer=False,
         batch_size=200,
     ):
+    """ Performs one epoch in the environment, updates the gvfs and the actor network """
         batch_acts = []  # for actions
         batch_behs = []  # for behaviors
         batch_weights = []  # for R(tau) weighting in policy gradient
@@ -53,12 +57,13 @@ class Agent:
 
         while True:
 
-            self.state = env.maze.objects.agent.positions[0] * 1
-            r_pos = env.maze.objects.goal.positions[0] * 1
-            self.action = self.controller(gvfs)
-            _, rew, done, _ = env.step(self.action)
-            pos_next = env.maze.objects.agent.positions[0] * 1
+            self.state = env.maze.objects.agent.positions[0] * 1 #position
+            r_pos = env.maze.objects.goal.positions[0] * 1 #goal
+            self.action = self.controller(gvfs) #action
+            _, rew, done, _ = env.step(self.action) #one step in the environment
+            pos_next = env.maze.objects.agent.positions[0] * 1 #next position
 
+            ###### The primitives are required to update the gvfs, the prims. come from the actor net #############
             output_actor = torch.reshape(
                 actor(torch.FloatTensor([1]).to("cpu")), (4, 4)
             )
@@ -74,7 +79,9 @@ class Agent:
                 primitives[prim, :] = primitives[prim, :] / np.sum(primitives[prim, :])
 
             gvfs.primitives = primitives
+            ############################################
 
+            # Update the GVFs
             utility = (pos_next[0] == r_pos[0] and pos_next[1] == r_pos[1]) * 1
             if self.exploration is True:
                 gvfs.td_backup(self.state, self.action, pos_next, utility)
@@ -108,8 +115,9 @@ class Agent:
                 if len(batch_acts) > batch_size:
                     break
 
-        if self.exploration is False:
 
+        # Train the actor after the batch is collected
+        if self.exploration is False:
             actor.train_actor(
                 batch_behs,
                 batch_acts,
