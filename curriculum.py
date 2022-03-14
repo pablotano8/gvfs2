156138ca42d161c4b887969cd0694432381cb869
pablotanoretamales <pablotanoretamales@gmail.com>
2022-03-14 10:18:08 +0000

    "33"

--- curriculum.py
+++ curriculum.py
@@ -3,17 +3,19 @@ from environment import Env
 
 
 class Curriculum:
-    def __init__(self, tasks: list, task_length: list) -> None:
+    def __init__(self, tasks: list, tasks_length: list) -> None:
         self.tasks = tasks
         self.tasks_length = tasks_length
 
 
 class AgentCuriculum:
-    def __init__(self, agent: Agent, curriculum: Curriculum, env=Env) -> None:
+    def __init__(self, agent: Agent, curriculum: Curriculum, env: Env) -> None:
         self.agent = agent
         self.curriculum = curriculum
         self.env = env
 
     def train_agent_on_task(self, task):
-        for i in range(self.curriculum.tasks_length[task]):
-            self.agent.train_one_epoch()
+        for _ in range(self.curriculum.tasks_length[task]):
+            self.agent.train_one_epoch(
+                self.env,
+            )
