5368dd0522ab0cfce12fc4ff4a503d97b0c56082
pablotanoretamales <pablotanoretamales@gmail.com>
2022-03-14 09:51:09 +0000

    4rd

---
+++ curriculum.py
@@ -0,0 +1,19 @@
+from agent import Agent
+from environment import Env
+
+
+class Curriculum:
+    def __init__(self, tasks: list, task_length: list) -> None:
+        self.tasks = tasks
+        self.tasks_length = tasks_length
+
+
+class AgentCuriculum:
+    def __init__(self, agent: Agent, curriculum: Curriculum, env=Env) -> None:
+        self.agent = agent
+        self.curriculum = curriculum
+        self.env = env
+
+    def train_agent_on_task(self, task):
+        for i in range(self.curriculum.tasks_length[task]):
+            self.agent.train_one_epoch()
